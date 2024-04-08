f3482af5d5adae1099f98387de3effcd711d4e29
Jayakrishnan <159559992+Jayakrishnanpdm@users.noreply.github.com>
2024-04-08 19:59:08 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-let burger_icon = document.querySelector('.burger-icon');
-let ul = document.querySelector("nav ul");
-burger_icon.addEventListener("click",()=>{
-    ul.classList.toggle("active");
-})
